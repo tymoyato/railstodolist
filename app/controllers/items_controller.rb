738e66bb6153e5d06fbab2177a60bdbb92c6924e
Ervin <duttoobadree@gmail.com>
2019-09-13 15:22:27 +0200

    add front

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,27 +1,30 @@
 class ItemsController < ApplicationController
-  def index
-    @items = Item.all
-  end
-
-  def show
-    @item = Item.find_by(id: params[:id])
-  end
-
   def new
     @item = Item.new
+    @items = Item.where(user_id: current_user.id)
   end
 
   def create
-    @item = Item.new
+    @item = Item.new(item_params)
 
     if @item.save
-      redirect_to @item
+      redirect_to new_item_path
     else
       render 'new'
     end
   end
 
-  def edit
+  def destroy
     @item = Item.find_by(id: params[:id])
+    if @item.present?
+      @item.destroy
+    end
+    redirect_to new_item_path
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :list_id, :user_id)
   end
 end
